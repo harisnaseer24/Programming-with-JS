930630b744fbc5f0420dffe77a89091c09f07a22
harisnaseer24 <harisnaseer258@gmail.com>
2023-08-18 11:10:53 +0500

    Modules and updated work

---
+++ modules/app.js
@@ -0,0 +1,5 @@
+import {a,b} from './main.js'
+import {c, greet} from './third.js'
+
+alert(a+b+c);
+greet();
---
+++ modules/third.js
@@ -0,0 +1,6 @@
+let c=19;
+function greet(){
+    let name=prompt("What's your name..?")
+    alert(`Asalam O Alaikum ${name} . Jumma Mubarak.`)
+}
+export {c, greet}
