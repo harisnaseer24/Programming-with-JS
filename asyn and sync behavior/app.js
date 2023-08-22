29b8f175838dcaa22d626135740080ffffb8516e
harisnaseer24 <harisnaseer258@gmail.com>
2023-08-22 13:46:38 +0500

    Async and sync in JS

---
+++ asyn and sync behavior/app.js
@@ -0,0 +1,36 @@
+// console.log("hi");
+
+// console.log("hi and hello");
+// console.log("hello");
+// console.log("bye");
+
+
+
+// function returnGreet(name){
+// console.log("hello "+ name);
+
+// setTimeout(()=>{
+//   console.log("enjoy in "+ name)
+// },2000)
+//  console.log("bye "+ name);
+
+// }
+
+// returnGreet("Aptech");
+
+// Async Await
+
+async function add(){
+    let a=14;
+    let b=64;
+    let c= a + b;
+   await console.log("your answer is ...");
+    console.log(c);
+}
+
+console.log("please wait")
+add();
+
+console.log("here is your answer");
+console.log("ye lo answer");
+
